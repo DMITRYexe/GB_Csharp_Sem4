d7ce4fea261c79c825d69828c82dd127b9df6e8b
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-11 00:28:45 +0300

    Project 1. Swap

--- Project1_Swap/Program.cs
+++ Project1_Swap/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Свап чисел местами
+int a = 4, b = 8;
+Console.WriteLine($"{a} и {b}");
+void Swap(int one, int two) // передача по значению (value) (a и b не меняются. меняются только one и ецщ)
+{
+  int t;
+  t = one;
+  one = two;
+  two = t;
+}
+Swap(a,b);
+Console.WriteLine($"{a} и {b}");
+void Swap2(ref int one, ref int two) // передача по ссылке ( references )
+{
+  int t;
+  t = one;
+  one = two;
+  two = t;
+}
+Swap2(ref a, ref b);
+Console.WriteLine($"{a} и {b}");
+// если мы не хотим исп. 3юю перменную:
+a = a + b;
+b = a - b;
+a = a - b;
+Console.WriteLine($"{a} и {b}");
