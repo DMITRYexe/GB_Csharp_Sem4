e6d172f9c56c616a709fb99d1a4cf6504b6e38d9
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-11 03:53:11 +0300

    Project 4. Array

---
+++ Project4_Array/Program.cs
@@ -0,0 +1,14 @@
+﻿// Работа с массивами данных
+// Создадим масштабируемый массив, в который мы можем сами вводить данные:
+
+double [] temperature = new double [3]; // инициаллизируем массив и задаем ему размер
+for (int i = 0; i < temperature.Length; i++) // цикл для ввода данных
+{
+ temperature[i]=int.Parse(System.Console.ReadLine());
+}
+double sum=0;
+for (int i = 0; i < temperature.Length; i++) // цикл для подсечта суммы чисел массива
+{
+ sum = sum + temperature[i];
+}
+System.Console.WriteLine($"{sum/temperature.Length}"); // находим и выводим ср. арифметическое
