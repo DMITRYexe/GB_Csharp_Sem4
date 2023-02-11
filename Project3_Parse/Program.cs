5964cdabec318e7bb5432c7019e76981f3f85597
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-11 03:17:23 +0300

    Project 3. Parse

---
+++ Project3_Parse/Program.cs
@@ -0,0 +1,15 @@
+﻿// Знакомство с оператором Parse.
+int a;
+bool f;
+do
+{
+f= int.TryParse( Console.ReadLine(), out a);
+if (f == false)
+{
+  System.Console.WriteLine("Error");
+}
+else
+{
+  System.Console.WriteLine($"{a}");
+}
+} while (f == false);
