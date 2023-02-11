d9b5006f0ac23900b16033596cd6ce24e94516cc
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-11 03:13:03 +0300

    Project 2. Try and Catch

---
+++ Project2_TryCatch/Program.cs
@@ -0,0 +1,18 @@
+﻿// Проверка правильности выполнения команды
+try
+{
+int i = Convert.ToInt32(Console.ReadLine());
+}
+catch
+{
+System.Console.WriteLine("Error!");
+}
+
+try
+{
+int i = Convert.ToInt32(Console.ReadLine());
+}
+catch (Exception ex)
+{
+System.Console.WriteLine(ex.Message);
+}
