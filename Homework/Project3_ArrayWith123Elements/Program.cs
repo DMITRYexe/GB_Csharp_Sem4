891c9d702eac39db778891a8242fa507977a028c
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-17 01:34:13 +0300

    Homework. Project 3. Array with 123 elements and checking digits againts a range [10;99]

---
+++ Homework/Project3_ArrayWith123Elements/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Создать массив из 123 элементов. Заполнить случайными положительными числами от 0 до 1000.
+Найти количество элементов которые принадлежат отрезку [10,99]. */
+Console.WriteLine("Привет! Эта программа создает массив из 123 числе и заполняет его числами от 0 до 1000.");
+System.Console.WriteLine("Затем она находит сколько чисел принадлежат отрезку [10,99]");
+int [] Array123 = new int [123];
+Random rndm = new Random();
+ArrayInit(Array123);
+PrintArray(Array123);
+System.Console.WriteLine();
+System.Console.WriteLine($"Количество чисел из диапазона [10;99]: {CounterDigitsAt10To99(Array123)}");
+
+
+int [] ArrayInit (int [] array)
+{
+  for (int i = 0; i<array.Length; i++)
+  {
+    array[i]=rndm.Next(0,1001);
+  }
+  return array;
+}
+
+void PrintArray (int [] array)
+{
+  for (int i=0; i < array.Length; i ++)
+  {
+    System.Console.Write($"{array[i]} ");
+  }
+}
+
+int CounterDigitsAt10To99 (int [] array)
+{
+  int counter = 0;
+    for (int i = 0; i<array.Length; i++)
+  {
+    if (array[i]>=10 && array[i]<=99)
+    {
+      counter++;
+    }
+  }
+  return counter;
+}
