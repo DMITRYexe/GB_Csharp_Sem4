51d9c40018f20a620a3fa8a2d0e104fe33c60d06
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-17 00:45:21 +0300

    Homework. Project 1. Array with 8 elements

---
+++ Homework/Project1_ArrayWith8Digits/Program.cs
@@ -0,0 +1,23 @@
+﻿//41. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+Console.WriteLine("Привет! Данная программа выводит массив из 8 элементов");
+int [] array = new int [8];
+ArrayIniciallization(array);
+PrintArray(array);
+
+int [] ArrayIniciallization (int [] a)
+{
+for (int i = 0; i<array.Length-1; i++)
+{
+  if (i%2==0) array[i]=1;
+  else array[i]=0;
+}
+return a;
+}
+
+void PrintArray (int [] a)
+{
+for (int i = 0; i<array.Length; i++)
+{
+System.Console.Write($"{array[i]} ");
+}
+}
